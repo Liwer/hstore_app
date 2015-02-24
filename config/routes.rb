4c563f4a0fa586e49e54deb62f804875a94a2327
eugene <zenya.09@gmail.com>
2015-02-24 13:39:19 +0200

    added show route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'products#index'
+  get 'products/:id' => 'products#show', as: 'show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
