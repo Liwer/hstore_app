82bb4b3145480d82492dd59085d7d1c26d7a9698
eugene <zenya.09@gmail.com>
2015-02-25 20:24:51 +0200

    add to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,17 +10,38 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
-    params[:order][:count] = 1 if params[:order][:count] == ""
-    if session[:cart].nil?
-      session[:cart] = []
+    order = params[:order]
+    add = Proc.new { |p| session[:cart] << { product_id: p[:product_id], options: [{ option_id: p[:option_id], count: p[:count] }] } }
+    current_product = Proc.new { |cart| cart.select { |p| p['product_id'] == order[:product_id] } }
+    current_option = Proc.new { |options| options.select { |h| h['option_id'] == order[:option_id] } }
+    session[:cart] = [] if session[:cart].nil?
+
+    if session[:cart].empty?
+      add.call(order)
+    else
+      # Adding option if product exist
+      unless current_product.call(session[:cart]).empty?
+        session[:cart].each do |product|
+          if product['product_id'] == order[:product_id]
+            # Adding option if same option exist
+            unless current_option.call(product['options']).empty?
+              product['options'].each do |option|
+                option['count'] = (option['count'].to_i + order[:count].to_i).to_s if option['option_id'] == order[:option_id]
+              end
+            else
+              product['options'] << { 'option_id' => order[:option_id], 'count' => order[:count] }
+            end
+          end
+        end
+      else
+        add.call(order)
+      end
     end
-    session[:cart] << {product_id: params[:order][:product_id], option: params[:order][:option], 
-                       count: params[:order][:count]}
     redirect_to :back
   end
 
   def clear_cart
-    session[:cart] = []
+    session.delete(:cart)
     redirect_to :back
   end
 
