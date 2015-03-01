f2886af8b82de90eca854d3b56e64c56ab2c42b5
eugene <zenya.09@gmail.com>
2015-03-01 19:27:30 +0200

    works with products block

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @products = Product.all.desc('_id').limit(8).to_a.each_slice(2).to_a
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def price(product)
+    product['options'].map { |p| p['price'].to_i }.min
+  end
 end
